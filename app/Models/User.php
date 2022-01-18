c06bdc509da263906b06d1da906e6f95f64e05ed
rizalb7 <rizalbaihaqi5@gmail.com>
2022-01-18 16:41:22 +0700

    make&set middleware role admin user

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,14 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function hasRole($role)
+    {
+        // check param $role dengan field usertype
+        if ($role == $this->usertype) {
+            return true;
+        }
+        return false;
+    }
     /**
      * The attributes that are mass assignable.
      *
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth', 'role:superadmin']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
